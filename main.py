66410b9cf22fc3e42fe6fd907e43ed22b68bfd3f
roshkhan2000 <Roshaank2000@gmail.com>
2026-04-29 17:24:44 +0100

    Create a designated folder for all JSON files

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 import requests as r
 import json 
 import datetime as dt
+import os
+
+# get current directory and folder to save files to 
+current_directory = os.getcwd()
+folder = os.path.join(current_directory, "BikePoints_JSON_Files")
+# make sure the folder exists
+os.makedirs(folder, exist_ok=True)
 
 # set variables for api call and make that call using get
 url = f"https://api.tfl.gov.uk/BikePoint"
@@ -11,17 +18,20 @@ response = r.get(url)
 if (response.status_code) == 200:
     # if response is 200 then convert response to json
     data = response.json()
+
     # for each dictionary in the list, get current time
     # also, get the name of the BikePoint using record.get("id")
     for record in data:
         time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         file_name = f"{record.get("id")}_{time}.json"
+        
         # create a json file in my folder
         # the w specifies that if a file does not exist, then create it, otherwise just open it
         # then dump the data resposne in to that file
-        with open(file_name, "w") as file:
+        with open(os.path.join(folder,file_name), "w") as file:
             json.dump(record, file)
         print(f"File {file_name} was successfully created!")
+
 # otherwise throw the below error
 else:
     print(f"Error creating files: {response.status_code}")
