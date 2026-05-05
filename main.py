aa7b4b725808d86e99b5d2283e60f7ec058eb88b
roshkhan2000 <Roshaank2000@gmail.com>
2026-05-05 09:37:28 +0100

    iteration removed from script, now calls back only 1 file with all the data

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-# import libraries and modules
+# import libs
 import requests as r
 import json 
 import datetime as dt
 import os
 
-# get current directory and folder to save files to 
-current_directory = os.getcwd()
-folder = os.path.join(current_directory, "BikePoints_JSON_Files")
-# make sure the folder exists
-os.makedirs(folder, exist_ok=True)
+# get current time, directory, and folder to save files to 
+time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+folder = os.path.join(os.getcwd(), "BikePoints_JSON_Files")
 
-# set variables for api call and make that call using get
-url = f"https://api.tfl.gov.uk/BikePoint"
+# set url for API and make the call
+# store the status of the API call
+url = f"https://api.tfl.gov.uk/BikePoint/"
 response = r.get(url)
+response_code = response.status_code
 
-# error handling
-if (response.status_code) == 200:
-    # if response is 200 then convert response to json
+# if response starts with 2 then do the following
+if response_code == 200:
+
+    # convert API respoinse to JSON
     data = response.json()
+    # create a folder and file to save if it doesn't exist already
+    os.makedirs(folder, exist_ok=True)
+    file_name = f"{time}.json"
+    
+    # open the file and dump the data in to it
+    with open(os.path.join(folder,file_name), "w") as file:
+        json.dump(data, file)
+    print(f"File {file_name} was successfully created!")
 
-    # for each dictionary in the list, get current time
-    # also, get the name of the BikePoint using record.get("id")
-    for record in data:
-        time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        file_name = f"{record.get("id")}_{time}.json"
-        
-        # create a json file in my folder
-        # the w specifies that if a file does not exist, then create it, otherwise just open it
-        # then dump the data resposne in to that file
-        with open(os.path.join(folder,file_name), "w") as file:
-            json.dump(record, file)
-        print(f"File {file_name} was successfully created!")
-
-# otherwise throw the below error
+# if response does not start with 2 then give an error
 else:
     print(f"Error creating files: {response.status_code}")
