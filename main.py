d35fef62d459f8f8ed59736cca48edb6f7c16350
roshkhan2000 <Roshaank2000@gmail.com>
2026-05-05 09:59:18 +0100

    API error handling and retries added + .env file updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests as r
 import json 
 import datetime as dt
 import os
+import time
 
 # get current time, directory, and folder to save files to 
 time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -14,20 +15,33 @@ url = f"https://api.tfl.gov.uk/BikePoint/"
 response = r.get(url)
 response_code = response.status_code
 
-# if response starts with 2 then do the following
-if response_code == 200:
+# create vars for API call retry (if it fails)
+count = 0 
+max_tries = 3
 
-    # convert API respoinse to JSON
-    data = response.json()
-    # create a folder and file to save if it doesn't exist already
-    os.makedirs(folder, exist_ok=True)
-    file_name = f"{time}.json"
-    
-    # open the file and dump the data in to it
-    with open(os.path.join(folder,file_name), "w") as file:
-        json.dump(data, file)
-    print(f"File {file_name} was successfully created!")
+while count < max_tries:
+    # if response is within the 200s then do the following
+    if 200 <= response_code < 300:
 
-# if response does not start with 2 then give an error
-else:
-    print(f"Error creating files: {response.status_code}")
+        # convert API respoinse to JSON
+        data = response.json()
+        # create a folder (if it doesn't exist already) and file to save 
+        os.makedirs(folder, exist_ok=True)
+        file_name = f"{time}.json"
+        
+        # open the file and dump the data to it
+        with open(os.path.join(folder,file_name), "w") as file:
+            json.dump(data, file)
+        print(f"File {file_name} was successfully created!")
+        break
+
+    # if respionse code is in the 500s then retry the API call
+    elif response_code >= 500:
+        time.sleep(10)
+        print(f"Response {response_code}: Trying again.. Attempt {count}")
+        count += 1
+
+    # if response code is not in the 200s, then print an error message
+    else:
+        print(f"Error creating files: {response_code}")
+        break
