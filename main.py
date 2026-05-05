cdd033eb129d73c4f56e590d5fe0fc9df40d3d7c
roshkhan2000 <Roshaank2000@gmail.com>
2026-05-05 10:34:36 +0100

    logger added and .env updated

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ import json
 import datetime as dt
 import os
 import time
+import logging
 
-# get current time, directory, and folder to save files to 
-time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-folder = os.path.join(os.getcwd(), "BikePoints_JSON_Files")
+# create a logging config, directory, and timestamp
+log_dir = f"{os.getcwd()}/logs"
+os.makedirs(log_dir, exist_ok=True)
+timestamp = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+log_filename = f"{log_dir}/{timestamp}.log"
+
+logging.basicConfig(
+    filename=log_filename,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+logger = logging.getLogger()
+logger.info('Logger Initialised')
+
+# create folder to save files to 
+folder = os.path.join(os.getcwd(), "JSON_Files")
 
 # set url for API and make the call
 # store the status of the API call
@@ -27,21 +42,21 @@ while count < max_tries:
         data = response.json()
         # create a folder (if it doesn't exist already) and file to save 
         os.makedirs(folder, exist_ok=True)
-        file_name = f"{time}.json"
+        file_name = f"{timestamp}.json"
         
         # open the file and dump the data to it
         with open(os.path.join(folder,file_name), "w") as file:
             json.dump(data, file)
-        print(f"File {file_name} was successfully created!")
+        logger.info(f"{file_name} was successfully created!")
         break
 
     # if respionse code is in the 500s then retry the API call
     elif response_code >= 500:
         time.sleep(10)
-        print(f"Response {response_code}: Trying again.. Attempt {count}")
+        logger.info(f"Response {response_code}: Trying again.. Attempt {count}")
         count += 1
 
     # if response code is not in the 200s, then print an error message
     else:
-        print(f"Error creating files: {response_code}")
+        logger.error(f"Error creating files: {response_code}")
         break
