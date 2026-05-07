efd840352e049564d1e4ef56847f7cc18c668243
roshkhan2000 <Roshaank2000@gmail.com>
2026-05-07 10:04:53 +0100

    setup logging module created

---
+++ modules/setup_logging.py
@@ -0,0 +1,25 @@
+import os 
+import logging
+import datetime as dt
+
+def setup_logging(log_dir):
+    """
+    this function sets up the logging for all modules- less repetiton setting up logs
+    Args:
+        log_dir (string): the filepath that you want the logs file to sit in
+
+    Returns:
+        _type_: _description_
+    """
+    log_dir = f"{os.getcwd()}/logs"
+    os.makedirs(log_dir, exist_ok=True)
+    timestamp = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    log_filename = f"{log_dir}/{timestamp}.log"
+
+    logging.basicConfig(
+        filename=log_filename,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+
+    return logging.getLogger('main')
